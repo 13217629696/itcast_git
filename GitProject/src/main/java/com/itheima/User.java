98e328ab3379034bea32361d85c1accc55f7f40f
denglixin <web24017683@163.com>
2018-12-15 09:41:07 +0800

    所有工程都提交

---
+++ GitProject/src/main/java/com/itheima/User.java
@@ -0,0 +1,38 @@
+package com.itheima;
+
+/**
+ * Created with IDEA
+ * User:龙猫
+ * Date:2018/12/15 0015
+ * Time:9:35
+ */
+public class User {
+
+    private String name;
+    private int  age;
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
